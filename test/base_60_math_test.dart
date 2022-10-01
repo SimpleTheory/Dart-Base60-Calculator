b3331cc8a80bcff5700a3cf1acca73917368084e
Ari Frid <arigfrid@hotmail.com>
2022-10-01 01:52:54 -0700

    finished abs test now for the longer one QQ

--- test/base_60_math_test.dart
+++ test/base_60_math_test.dart
@@ -47,46 +47,40 @@ void main() {
     test('Abs60<', () {
         expect(a<b, isTrue);
       });
-    test('Abs60<, 2', () {
-        expect(a<c, isTrue);
-      });
     test('Abs60< Falsey', () {
-        expect(b<a, isFalse);
+        expect(a<c, isFalse);
+      });
+    test('Abs60< Falsey, 2', () {
+        expect(b<a, isFalse);});
+    
+    test('abs+0', () {
+      Abs60 a1 = base_60_math.AbsBase60.from_integer(437);
+      Abs60 b1 = base_60_math.AbsBase60.zero();
+      expect(a1+b1, equals(Abs60 a1));
+    });
+    test('abs +6', () {
+      Abs60 a1 = base_60_math.AbsBase60.from_integer(437);
+      Abs60 c1 = base_60_math.AbsBase60.from_integer(6);
+      expect(a+c, equals(base_60_math.AbsBase60.from_integer(443));
+    });
 
+    test('abs to double', () {
+      expect(base_60_math.AbsBase60.from_commas('1,16;15').toDouble(),
+          equals(76.25));
+    });
 
+    test('from double to abs', () {
+      expect(base_60_math.AbsBase60.from_double(76.25),
+          equals(base_60_math.AbsBase60.from_commas('1,16;15')));
+    });
+    test('from double to abs 1/3', () {
+      expect(base_60_math.AbsBase60.from_double(1/3),
+          equals(base_60_math.AbsBase60.from_commas(';20')));
+    });
 
-  def test_abs_lt():
-  assert a < b
-
-  assert (a < c) is False
-  assert not (b < a)
-
-
-  def test_abs_add():
-  a = base_60_math.AbsBase60.from_integer(437)
-  b = base_60_math.AbsBase60.zero()
-  assert a + b == a
-  c = base_60_math.AbsBase60.from_integer(6)
-  d = 6 + 437
-  assert c + a == base_60_math.AbsBase60.from_integer(d)
-
-
-  def test_abs_to_float():
-  assert float(base_60_math.AbsBase60.from_commas('1,16;15')) == 76.25
-
-
-  def test_abs_round():
-  assert base_60_math.AbsBase60.from_commas('2;59,59,59,59,30').round() \
-  == base_60_math.AbsBase60.from_integer(3)
-
-
-  def test_abs_from_float():
-  assert base_60_math.AbsBase60.from_float(76.25) == base_60_math.AbsBase60.from_commas('1,16;15')
-
-
-  def test_abs_from_float_thirds():
-  assert base_60_math.AbsBase60.from_float(1 / 3) == base_60_math.AbsBase60.from_commas(';20
-  '
+    test('abs round', (){
+      Abs60 c1 = base_60_math.AbsBase60.from_commas('2;59,59,59,59,30').round();
+      expect(c1, equals(base_60_math.AbsBase60.from_integer(3)));
+    });
   });
-
 }
