81fbff3e205385cb48134f78c2feea901fbcd7e4
Ari Frid <arigfrid@hotmail.com>
2022-10-08 00:37:06 -0700

    started writing actual code made range func for future help and program in tests to try stuff out

---
+++ lib/math/base_60_math.dart
@@ -0,0 +1,20 @@
+// Imports
+
+// Meta-Utility ----------------------------------------------------------------
+List<int> range(int stop, {int? start, int? step}){
+  step ??= 1;
+  start ??= 0;
+  List<int> range = [];
+  for (int i = start; i < stop; i+=step){
+    range.add(i);
+  }
+  return range;
+}
+List<T> reverse<T>(List<T> x) => List<T>.from(x.reversed);
+List<T> sorted<T>(List<T> x){List<T> y = List.from(x); y.sort(); return y;}
+
+// generate tab  --->  ctrl + shift + g
+// KMS--------------------------------------------------------------------------
+
+
+
