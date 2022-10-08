639a57009ab5c533022712539b75ea3805447752
Ari Frid <arigfrid@hotmail.com>
2022-10-07 20:54:15 -0700

    did some work on compare upto prepcompare some more to go tho b4 sort and wholenumber class tests

--- test/base_60_math_test.dart
+++ test/base_60_math_test.dart
@@ -37,19 +37,19 @@ void main() {
       // [0, 59,59,3,  5]
       // [1, 2, 3, 4,  5]
       // [2, 2, 2, 7, 10]
-      expect(listEquals(c.number, [2, 2, 2, 7, 10]), isTrue);
+      expect(listEquals(c.number, [2, 2, 2, 7, 10]), true);
     });
     test('add items in base number list (base60): carryover', (){
-      expect((listEquals(base_60_math.add_items_in_list_number(
-          [59, 59, 59, 59], [59, 59, 59, 59]), [1, 59, 59, 59, 58]), isTrue));
+      expect(listEquals(base_60_math.add_items_in_list_number(
+          [59, 59, 59, 59], [59, 59, 59, 59]), [1, 59, 59, 59, 58]), true);
     });
     test('add items in base number list (base60) 2', (){
       List<int> a = [7,17];
       List<int> b = [6];
       expect(listEquals(
-          base_60_math.add_items_in_list_number(a, b), [7, 23]), isTrue);
+          base_60_math.add_items_in_list_number(a, b), [7, 23]), true);
       expect(listEquals(
-          base_60_math.add_items_in_list_number(b, a), [7, 23]), isTrue);
+          base_60_math.add_items_in_list_number(b, a), [7, 23]), true);
     });
     test('add items in a list fraction', (){
       expect(base_60_add_items_in_list_fraction([30, 40, 20], [5, 5, 40])),
@@ -107,11 +107,11 @@ void main() {
       expect(b.abs(), equals(expected.abs()));
     });
     test('Negative true', (){
-      expect(a.negative, isTrue());
+      expect(a.negative, true);
     });
     test('Negative false', (){
 
-      expect(b.negative, isFalse());
+      expect(b.negative, false);
     });
   });
   group('Lazy integrated addition', (){
@@ -127,7 +127,7 @@ void main() {
       AbsBase60 a = base_60_math.AbsBase60.from_integer(6);
       AbsBase60 b = base_60_math.AbsBase60.from_integer(437);
       expected = base_60_math.AbsBase60.from_integer(int_);
-      expect(base_60_math.lazy_addition(a, b) == base_60_math.lazy_addition(b, a), true);
+      expect(base_60_math.lazy_addition(a, b), base_60_math.lazy_addition(b, a));
     });
     test('lazy add int', (){
       int int_ = 6 + 437;
@@ -138,103 +138,118 @@ void main() {
     });
   });
 // -----------------------------------------------------------------------------
-  group(comparator, () => null)
+  group('comparator', (){
+    test('truthy', (){
+      expect(base_60_math.comparator([30, 27], [59]), [true, false]);
+      expect(base_60_math.comparator([30, 27], [19, 39]), [true, false]);
+      expect(base_60_math.comparator([30, 27], [30, 26]), [true, false]);
+      expect(base_60_math.comparator(reverse([20, 40, 30]), reverse([40, 5, 5]), true), [true, false]);
+      expect(base_60_math.comparator([30, 27], [30, 27]), [false, true]);
+    });
+    test('falsey', (){
+      expect(base_60_math.comparator([59], [30, 27]), [false, false]);
+      expect(base_60_math.comparator([1, 59], [30, 27]), [false, false]);
+      expect(base_60_math.comparator([30, 26], [30, 27]), [false, false]);
+    });
+    test('precompare frac', (){
+      List<int> a = base_60_math.prep_compare([1, 2, 1, 1], [1, 1], true);
+      expect(a, [[1, 2, 1, 1], [0, 0, 1, 1]]);
+      });
+    test('prepcompare int', (){
+      List<int> b = base_60_math.prep_compare([1, 2, 1, 1], [1, 1], false);
+      expect(b, [[1, 2, 1, 1], [1, 1, 0, 0]]);
+    });
+
+    test('', (){
+
+    });
+    test('', (){
+
+    });
+    test('', (){
+
+    });
+    test('', (){
+
+    });
+  });
 
 
 
   //////////////////////////////
 
-
-
-  def test_comparator_truthy():
-  assert base_60_math.comparator([30, 27], [59]) == (True, False)
-  assert base_60_math.comparator([30, 27], [19, 39]) == (True, False)
-  assert base_60_math.comparator([30, 27], [30, 26]) == (True, False)
-  assert base_60_math.comparator(reverse([20, 40, 30]), reverse([40, 5, 5]), True) == (True, False)
-  assert base_60_math.comparator([30, 27], [30, 27]) == (False, True)
-
-
-  def test_comparator_falsey():
-  assert base_60_math.comparator([59], [30, 27]) == (False, False,)
-  assert base_60_math.comparator([1, 59], [30, 27]) == (False, False,)
-  assert base_60_math.comparator([30, 26], [30, 27]) == (False, False,)
-
-
   def test_prep_compare():
-  a = base_60_math.prep_compare([1, 2, 1, 1], [1, 1], True)
-  b = base_60_math.prep_compare([1, 2, 1, 1], [1, 1], False)
-  assert b == ([1, 2, 1, 1], [1, 1, 0, 0],)
-  assert a == ([1, 2, 1, 1], [0, 0, 1, 1],)
 
-  assert base_60_math.prep_compare([1, 2, 1, 1], [1, 1], True, True) == tuple([reverse(i) for i in a])
-  assert base_60_math.prep_compare([1, 2, 1, 1], [1, 1], False, True) == tuple([reverse(i) for i in b])
+
+  expect(base_60_math.prep_compare([1, 2, 1, 1], [1, 1], true, true), tuple([reverse(i) for i in a])
+  expect(base_60_math.prep_compare([1, 2, 1, 1], [1, 1], false, true), tuple([reverse(i) for i in b])
 
 
   def test_int_to_base():
-  a = base_60_math.int_to_base(478, 60)
-  assert a == [7, 58]
+  List<int> a = base_60_math.int_to_base(478, 60)
+  expect(a, [7, 58]
 
 
   def test_sort():
-  init_ = [base_60_math.AbsBase60.from_integer(i) for i in [661, 409, 7236, 1976, 2764]]
-  final = [base_60_math.AbsBase60.from_integer(i) for i in sorted([661, 409, 7236, 1976, 2764])]
-  assert base_60_math.sort(init_) == final
+  List<AbsBase60> init_ = [base_60_math.AbsBase60.from_integer(i) for i in [661, 409, 7236, 1976, 2764]]
+  List<AbsBase60> final = [base_60_math.AbsBase60.from_integer(i) for i in sorted([661, 409, 7236, 1976, 2764])]
+  expect(base_60_math.sort(init_), final
 
 
   def test_remove_0s_from_end():
-  assert base_60_math.remove_0s_from_end([23, 0, 0, 43, 4, 0, 0]) == [23, 0, 0, 43, 4]
-  assert base_60_math.remove_0s_from_end([0]) == []
+  expect(base_60_math.remove_0s_from_end([23, 0, 0, 43, 4, 0, 0]), [23, 0, 0, 43, 4]
+  expect(base_60_math.remove_0s_from_end([0]), []
 
 
   def test_wholenumberizer():
-  a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
-  b = a.wholenumberize()
-  assert b.number == [4, 16, 54, 8]
-  assert b.seximals == 2
+  AbsBase60 a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
+  List<intb = a.wholenumberize()
+  expect(b.number, [4, 16, 54, 8]
+  expect(b.seximals, 2
 
 
   def test_wholenumberizer_reverse():
-  a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
-  b = a.wholenumberize(True)
-  assert b.number == reverse([4, 16, 54, 8]) and b.reversed is True
+  AbsBase60 a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
+  WholeNumberBase60 b = a.wholenumberize(true)
+  expect(b.number, reverse([4, 16, 54, 8]) and b.reversed is true
 
 
   def test_wholenumber_self_reverse():
-  a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
-  b = a.wholenumberize(True)
+  AbsBase60 a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
+  b = a.wholenumberize(true)
   b.toggle_reverse()
-  assert b.number == [4, 16, 54, 8] and b.reversed is False
+  expect(b.number, [4, 16, 54, 8] and b.reversed is false
 
 
   def test_wholenumber_int():
   a = base_60_math.WholeBase60Number([1, 1, 6], 0)
   b = base_60_math.WholeBase60Number([1, 1, 6], 0)
-  assert int(a) == 3666
-  assert int(b) == 3666
+  expect(int(a), 3666
+  expect(int(b), 3666
 
 
   def test_inverse():
   a = base_60_math.inverse(base_60_math.AbsBase60.from_commas('2;30'))
-  assert a == base_60_math.AbsBase60.from_commas(';24')
+  expect(a, base_60_math.AbsBase60.from_commas(';24')
 
 
   def test_carry_over_reformat_base():
   a = base_60_math.carry_over_reformat_base([43, 108, 70, 67, 23, 137])
-  assert base_60_math.carry_over_reformat_base([60, 59, 59, 59, 59]) == [0, 0, 0, 0, 0, 1]
-  assert a == [43, 48, 11, 8, 24, 17, 2]
+  expect(base_60_math.carry_over_reformat_base([60, 59, 59, 59, 59]), [0, 0, 0, 0, 0, 1]
+  expect(a, [43, 48, 11, 8, 24, 17, 2]
 
 
   def test_wholenumber_to_abs():
   a = base_60_math.AbsBase60.from_commas('4,16;54,8,0')
   b = a.wholenumberize().to_Abs60()
-  assert b == a
+  expect(b, a
 
 
   def test_divide():
   a = base_60_math.AbsBase60.from_commas('2;30')
   b = base_60_math.AbsBase60.from_commas('1;30')
   c = base_60_math.lazy_division(a, b)
-  assert c == base_60_math.AbsBase60.from_commas('1;40')
+  expect(c, base_60_math.AbsBase60.from_commas('1;40')
 
 
 }
